be1d6b63c75875076cff2d97d9ad6c0d606959d9
冯家栋的win10 <oncwnuKtphSrnY8eyt2ns82OGwCw@git.weixin.qq.com>
2020-07-13 23:05:41 +0800

    测试提交
    test

--- miniprogram/pages/databaseGuide/databaseGuide.js
+++ miniprogram/pages/databaseGuide/databaseGuide.js
@@ -27,7 +27,7 @@ Page({
         count: 1
       },
       success: res => {
-        // 在返回结果中会包含新创建的记录的 _id
+        // 在返回结果中会包含新创建的记录的 _id test
         this.setData({
           counterId: res._id,
           count: 1
